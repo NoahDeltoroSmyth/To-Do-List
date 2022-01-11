45b096a96aa1e15984a42522af0ab761a7e5abf3
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-01-11 14:39:01 -0800

    Route set up for form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import './App.css';
+import { Route, Switch, BrowswerRouter } from 'react-router-dom';
+import { useState } from 'react';
+import { getUser, logout } from './services/userRoute';
+import Auth from './views/Auth';
 
 function App() {
-  return <div className="App"></div>;
+  const [currentUser, setCurrentUser] = useState(getUser());
+
+  const logoutUser = async () => {
+    await logout();
+    setCurrentUser(null);
+  };
+
+  return (
+    <div className="App">
+      <BrowswerRouter>
+        <Switch>
+          <Route exact path="/">
+            {currentUser && (
+              <>
+                <h1>You are signed in</h1>
+                <button onClick={logoutUser}>Logout</button>
+              </>
+            )}
+            {!currentUser && <Auth setCurrentUser={setCurrentUser} />}
+          </Route>
+        </Switch>
+      </BrowswerRouter>
+    </div>
+  );
 }
 
 export default App;
